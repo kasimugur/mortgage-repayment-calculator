ee60d6c63b10da80e4a52cf96456fc6b93404ba9
kasimugur <kasiugur7@gmail.com>
2024-07-23 17:56:10 +0300

    error type added

--- src/components/ActiveCard.jsx
+++ src/components/ActiveCard.jsx
@@ -1,4 +1,9 @@
+import { useContext } from "react"
+import SiteContext from "../context/SiteContext"
+
 export default function ActiveCard() {
+  const {total, monthly} = useContext(SiteContext)
+
   return (
     <>
     <h1>Your results</h1>
@@ -6,10 +11,10 @@ export default function ActiveCard() {
     To adjust the results, edit the form and click “calculate repayments” again.</p>
     <div className="card-active">
       <h5>Your monthly repayments</h5>
-      <h1>£ 0000 </h1>
+      <h1>£{monthly} </h1>
 
       <h5>  Total you'll repay over the term</h5>
-      <h3>£ 2132 </h3>
+      <h3>£ {total} </h3>
     </div>
     </>
   )
--- src/components/Mortgage.jsx
+++ src/components/Mortgage.jsx
@@ -4,8 +4,11 @@ import '../scss/style.scss'
 import { useEffect } from 'react'
 import DefaultCard from './DefaultCard'
 import ActiveCard from './ActiveCard'
+import SiteContext from '../context/SiteContext'
 
 export default function Mortgage() {
+
+
 
   const [mortgageTotal, setMortgageTotal] = useState('')
   const [mortgageTerm, setMortgageTerm] = useState('')
@@ -22,11 +25,6 @@ export default function Mortgage() {
     return topl + sayi
   }
 
-  useEffect(() => {
-    setMothly(total / mortgageTerm)
-
-  }, [total])
-
 
   const result = () => {
     if ((mortgageTerm && mortgageTotal && interest) === '') {
@@ -34,67 +32,82 @@ export default function Mortgage() {
       setError('error')
     } else {
       setTotal(interestRate(mortgageTotal, interest))
+      setMothly(total / mortgageTerm)
     }
   }
 
+  const clear = () => {
+    setTotal('')
+    setMortgageTerm('')
+    setMortgageTotal('')
+    setInterest('')
+    setError('')
+    setMothly('')
+  }
 
+  const data = {
+    total,
+    monthly
+  }
 
   return (
     <>
-      <div className="container">
-        <div className="card-item">
-          <div className="header">
-            <h3>Mortgage Calculator </h3>
-            <a>Clear All</a>
-          </div>
-          <form onSubmit={(e) => e.preventDefault()} className="form">
-            <div className="amount">
-              <label>Mortgage Amount</label>
-              <input
-                value={mortgageTotal} onChange={(e) => setMortgageTotal(Number(e.target.value))} type="number" />
-              <span>£</span>
-              {/* {(error === 'error' && !mortgageTotal) ? <p>This fıeld is required</p> : '' } */}
+      <SiteContext.Provider value={data} >
+        <div className="container">
+          <div className="card-item">
+            <div className="header">
+              <h3>Mortgage Calculator </h3>
+              <a onClick={clear}>Clear All</a>
             </div>
-            <div className="inpt">
-              <div className="inpt-term">
-                <label>Mortgage Term</label>
+            <form onSubmit={(e) => e.preventDefault()} className="form">
+              <div className={`amount ${!mortgageTotal ? error : ''}`}>
+                <label>Mortgage Amount</label>
                 <input
-                  value={mortgageTerm} onChange={(e) => setMortgageTerm(Number(e.target.value))}
-                  type="number" />
-                <span>years</span>
-                {/* <p>This fıeld is required</p> */}
+                  value={mortgageTotal} onChange={(e) => setMortgageTotal(Number(e.target.value))} type="number" />
+                <span>£</span>
+                {(error === 'error' && !mortgageTotal) ? <p>This fıeld is required</p> : '' }
               </div>
-              <div className="inpt-rate">
-                <label>Interest Rate</label>
-                <input
-                  value={interest} onChange={(e) => setInterest(Number(e.target.value))}
-                  type="number" />
-                <span>%</span>
-                {/* <p>This fıeld is required</p> */}
+              <div className="inpt">
+                <div className={`inpt-term ${!mortgageTerm ? error : ''}`}>
+                  <label>Mortgage Term</label>
+                  <input
+                    value={mortgageTerm} onChange={(e) => setMortgageTerm(Number(e.target.value))}
+                    type="number" />
+                  <span>years</span>
+                  {(error === 'error' && !mortgageTerm) ? <p>This fıeld is required</p> : '' }
+                </div>
+                <div className={`inpt-rate ${!interest ? error : ''}`}>
+                  <label>Interest Rate</label>
+                  <input
+                    value={interest} onChange={(e) => setInterest(Number(e.target.value))}
+                    type="number" />
+                  <span>%</span>
+                  {(error === 'error' && !interest) ? <p>This fıeld is required</p> : '' }
+                </div>
               </div>
-            </div>
-            <div className="type">
-              <label>Mortgage Type </label>
-              <div className="rad">
-                <input type="radio" />
-                <label>Repayment</label>
-              </div>
-              <div className="rad ">
-                <input type="radio" />
-                <label>Interest Only</label>
+              <div className="type">
+                <label>Mortgage Type </label>
+                <div className="rad">
+                  <input type="radio" />
+                  <label>Repayment</label>
+                </div>
+                <div className="rad ">
+                  <input type="radio" />
+                  <label>Interest Only</label>
 
+                </div>
+                {/* <p>This fıeld is required</p> */}
               </div>
-              {/* <p>This fıeld is required</p> */}
+              <button onClick={() => result()}  > {iconCalculator}Calculate Repayments</button>
+            </form>
+          </div>
+          <div className="card">
+            <div className="row">
+              {total === '' ? <DefaultCard /> : <ActiveCard />}
             </div>
-            <button onClick={() => result()}  > {iconCalculator}Calculate Repayments</button>
-          </form>
-        </div>
-        <div className="card">
-          <div className="row">
-            { total === '' ? <DefaultCard /> : <ActiveCard /> }
-        </div>
-      </div>
-    </div >
+          </div>
+        </div >
+      </SiteContext.Provider>
     </>
   )
 }
