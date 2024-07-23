1faed4ecfd94fef2badd1f0e5408af4f6238a7cd
kasimugur <kasiugur7@gmail.com>
2024-07-23 16:23:00 +0300

    mortgage added

--- src/components/Mortgage.jsx
+++ src/components/Mortgage.jsx
@@ -1,7 +1,37 @@
+import { useState } from 'react'
 import { iconCalculator, ilistrationEmpty } from '../page/page'
 import '../scss/style.scss'
+import { useEffect } from 'react'
 
 export default function Mortgage() {
+
+  const [mortgageTotal, setMortgageTotal] = useState('')
+  const [mortgageTerm, setMortgageTerm] = useState('')
+  const [interest, setInterest] = useState('')
+  const [monthly, setMothly] = useState('')
+  const [total, setTotal] = useState('')
+  const [error, setError] = useState('')
+
+
+  function interestRate(sayi, yüzde) {
+    Number(sayi)
+    Number(yüzde)
+    const topl = (sayi * yüzde / 100)
+    return topl + sayi
+  }
+
+  useEffect(() => {
+    setMothly(total / mortgageTerm)
+
+  }, [total])
+
+
+  const result = () => {
+    setTotal(interestRate(mortgageTotal, interest))
+  }
+
+
+
   return (
     <>
       <div className="container">
@@ -10,25 +40,30 @@ export default function Mortgage() {
             <h3>Mortgage Calculator </h3>
             <a>Clear All</a>
           </div>
-          <form className="form error">
+          <form onSubmit={(e) => e.preventDefault()} className="form">
             <div className="amount">
               <label>Mortgage Amount</label>
-              <input type="number"  />
+              <input
+                value={mortgageTotal} onChange={(e) => setMortgageTotal(Number(e.target.value))} type="number" />
               <span>£</span>
-              <p>This fıeld is required</p>
+              {/* <p>This fıeld is required</p> */}
             </div>
             <div className="inpt">
               <div className="inpt-term">
                 <label>Mortgage Term</label>
-                <input type="number"  />
+                <input
+                  value={mortgageTerm} onChange={(e) => setMortgageTerm(Number(e.target.value))}
+                  type="number" />
                 <span>years</span>
-                <p>This fıeld is required</p>
+                {/* <p>This fıeld is required</p> */}
               </div>
               <div className="inpt-rate">
                 <label>Interest Rate</label>
-                <input type="number" />
+                <input
+                  value={interest} onChange={(e) => setInterest(Number(e.target.value))}
+                  type="number" />
                 <span>%</span>
-                <p>This fıeld is required</p>
+                {/* <p>This fıeld is required</p> */}
               </div>
             </div>
             <div className="type">
@@ -42,15 +77,17 @@ export default function Mortgage() {
                 <label>Interest Only</label>
 
               </div>
-              <p>This fıeld is required</p>
+              {/* <p>This fıeld is required</p> */}
             </div>
-            <button> {iconCalculator}Calculate Repayments</button>
+            <button onClick={() => result()}  > {iconCalculator}Calculate Repayments</button>
           </form>
         </div>
         <div className="card">
           <div className="row">
             {ilistrationEmpty}
             <h2>  Results shown here</h2>
+            {monthly} <h3>: her ay</h3><br />
+            {total} <h3>: toplam</h3>
             <p>Complete the form and click “calculate repayments” to see what
               your monthly repayments would be.</p>
           </div>
